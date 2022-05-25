09837b206664b98fe7f15c9131a732dd4f50da4b
Jose Alexandre Viana Marques <jalexandrevm@gmail.com>
2022-05-25 15:55:55 -0300

    Finalizando projeto

--- tamarcado/settings/heroku.py
+++ tamarcado/settings/heroku.py
@@ -3,3 +3,17 @@ from tamarcado.settings.base import *
 import django_on_heroku
 
 django_on_heroku.settings(locals())
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND_PROD", "")
+EMAIL_HOST = os.environ.get("EMAIL_HOST_PROD", "")
+EMAIL_PORT = os.environ.get("EMAIL_PORT_PROD", "")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER_PROD", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD_PROD", "")
+EMAIL_USE_SSL = True
+EMAIL_USE_TSL = False
+
+CELERY_BROKER_URL = os.environ.get("REDIS_URL", "")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "")
